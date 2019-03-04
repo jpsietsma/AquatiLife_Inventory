1387b663e137856dc4bd8873890c56fc183af307
Jimmy S <jpsietsma@gmail.com>
2019-03-04 09:55:42 -0500

    Added accessoryType, FishFeederType, PurchaseLocationOptions, and PurchaseType.  Additionally added LiveFish object as base object

---
+++ AquatiLife Inventory/models/LiveFish.cs
@@ -0,0 +1,60 @@
+﻿using AquatiLife_Inventory.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace AquatiLife_Inventory.models
+{
+    /// <summary>
+    /// Represents a live fish
+    /// </summary>
+    public class LiveFish
+    {
+        /// <summary>
+        /// Represents the type of object, "LiveFish" has an "AccessoryType" of "Fish"
+        /// </summary>
+        public AccessoryType EntityType { get; set; } = AccessoryType.Fish;
+
+        /// <summary>
+        /// Human-Friendly pet name of the fish
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Describes the primary color of the fish
+        /// </summary>
+        public Color PrimaryColor { get; set; }
+
+        /// <summary>
+        /// Describes the secondary color of the fish
+        /// </summary>
+        public Color SecondaryColor { get; set; }
+
+        /// <summary>
+        /// Represents category of feeder type that fish falls into
+        /// </summary>
+        public FishFeederType FeederType { get; set; }
+
+        /// <summary>
+        /// Location where fish was purchased
+        /// </summary>
+        public PurchaseOptions PurchaseLocation { get; set; }
+
+        /// <summary>
+        /// Date fish was purchased or received
+        /// </summary>
+        public DateTime PurchaseDate { get; set; }
+
+        /// <summary>
+        /// Whether or not fish is still living
+        /// </summary>
+        public bool IsAlive { get; set; }
+
+
+
+
+    }
+}
---
+++ AquatiLife Inventory/models/TankAccessory.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatiLife_Inventory.models
+{
+
+    /// <summary>
+    /// Base class for all things, including living, added to a fish tank
+    /// </summary>
+    public class TankAccessory
+    {
+
+        /// <summary>
+        /// Database ID of the tank accessory
+        /// </summary>
+        public int AccessoryID { get; set; }
+
+        /// <summary>
+        /// Tank which the accessory belongs to
+        /// </summary>
+        public int TankID { get; set; }
+
+        /// <summary>
+        /// Name of the accessory as displayed to the user
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Cost of the accessory in double
+        /// </summary>
+        public double PurchaseCost { get; set; }
+
+        /// <summary>
+        /// Date the accessory was purchased
+        /// </summary>
+        public DateTime PurchaseDate { get; set; }
+
+        /// <summary>
+        /// Store or location where accessory was purchased
+        /// </summary>
+        public string PurchaseLocation { get; set; }
+
+        /// <summary>
+        /// Base class for a new accessory for a tank which includes plants, ornaments, decorations, leds, fish, filter, heater, air pump, bubble decorations, gravel, food, and chemicals
+        /// </summary>
+        /// <param name="accessoryName">Name of the accessory as displayed to the user</param>
+        /// <param name="accessoryCost">Cost of the accessory when purchased in $USD</param>
+        /// <param name="accessoryPurchaseDate">Date of the purchase</param>
+        public TankAccessory(string accessoryName, double accessoryCost, DateTime accessoryPurchaseDate)
+        {
+            DisplayName = accessoryName;
+            PurchaseCost = accessoryCost;
+            PurchaseDate = accessoryPurchaseDate;
+        }
+
+        /// <summary>
+        /// Base class for a new accessory for a tank which includes plants, ornaments, decorations, leds, fish, filter, heater, air pump, bubble decorations, gravel, food, and chemicals
+        /// </summary>
+        /// <param name="accessoryName">Name of the accessory as displayed to the user</param>
+        /// <param name="accessoryTankID">Tank which the accessory belongs to</param>
+        /// <param name="accessoryCost">Cost of the accessory when purchased in $USD</param>
+        /// <param name="accessoryPurchaseDate">Date of the purchase</param>
+        public TankAccessory(string accessoryName, int accessoryTankID, double accessoryCost, DateTime accessoryPurchaseDate)
+        {
+            DisplayName = accessoryName;
+            TankID = accessoryTankID;
+            PurchaseCost = accessoryCost;
+            PurchaseDate = accessoryPurchaseDate;
+        }
+
+        /// <summary>
+        /// Base class for a new accessory for a tank which includes plants, ornaments, decorations, leds, fish, filter, heater, air pump, bubble decorations, gravel, food, and chemicals
+        /// </summary>
+        /// <param name="accessoryName">Name of the accessory as displayed to the user</param>
+        /// <param name="accessoryCost">Cost of the accessory when purchased in $USD</param>
+        /// <param name="accessoryPurchaseDate">Date of the purchase</param>
+        /// <param name="accessoryPurchaseLocation">Store location where accessory was purchased</param>
+        public TankAccessory(string accessoryName, double accessoryCost, DateTime accessoryPurchaseDate, string accessoryPurchaseLocation)
+        {
+            DisplayName = accessoryName;
+            PurchaseCost = accessoryCost;
+            PurchaseDate = accessoryPurchaseDate;
+            PurchaseLocation = accessoryPurchaseLocation;
+        }
+
+        /// <summary>
+        /// Base class for a new accessory for a tank which includes plants, ornaments, decorations, leds, fish, filter, heater, air pump, bubble decorations, gravel, food, and chemicals
+        /// </summary>
+        /// <param name="accessoryName">Name of the accessory as displayed to the user</param>
+        /// <param name="accessoryTankID">Tank that the accesory belongs to</param>
+        /// <param name="accessoryCost">Cost of the accessory when purchased in $USD</param>
+        /// <param name="accessoryPurchaseDate">Date of the purchase</param>
+        /// <param name="accessoryPurchaseLocation">Store location where accessory was purchased</param>
+        public TankAccessory(string accessoryName, int accessoryTankID, double accessoryCost, DateTime accessoryPurchaseDate, string accessoryPurchaseLocation)
+        {
+            DisplayName = accessoryName;
+            TankID = accessoryTankID;
+            PurchaseCost = accessoryCost;
+            PurchaseDate = accessoryPurchaseDate;
+            PurchaseLocation = accessoryPurchaseLocation;
+        }
+    }
+}
